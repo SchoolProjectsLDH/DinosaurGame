a71bcf933b1bf49316baa5d00f29a2718c8f887f
Kggupta <kgupt1@ocdsb.ca>
2019-11-08 08:53:32 -0500

    revert to nimbus

--- src/userGUI/Lose.java
+++ src/userGUI/Lose.java
@@ -204,7 +204,7 @@ public class Lose extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }

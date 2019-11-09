b572403ebc0556b523347a5f27b0ee8fec655a31
Kggupta <kgupt1@ocdsb.ca>
2019-11-09 09:45:08 -0500

    commented game window maker

--- src/userGUI/GameCreator.java
+++ src/userGUI/GameCreator.java
@@ -4,27 +4,23 @@ import javax.swing.JFrame;
 
 public class GameCreator extends JFrame {
 	
-	public static final int SCREEN_WIDTH = 600;
-	private Screen screenPlayer;
+	public static final int SCREEN_WIDTH = 600;//create acctual game window
+	private Screen screenPlayer;//new player
         
 	
 	public GameCreator() {
-            super("T-Rex game");
+            super("Chrome Dino Game");//set name
 	}
 	
 	public void startGame(int speed, String UserName) {
-            setSize(SCREEN_WIDTH, 175);
-            setLocation(400, 200);
-            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
-            setResizable(false);
-            screenPlayer = new Screen(speed, UserName);
-            addKeyListener(screenPlayer);
-            add(screenPlayer);
-            setVisible(true);
-            screenPlayer.startGame();
+            setSize(SCREEN_WIDTH, 175);//size of window
+            setLocation(400, 200);//location on screen
+            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //dont exit program on close
+            setResizable(false);//not resizable
+            screenPlayer = new Screen(speed, UserName);//update screen with the speed and username
+            addKeyListener(screenPlayer);//get key input on screen player
+            add(screenPlayer);//place screen
+            setVisible(true);//make visible
+            screenPlayer.startGame();//start the game
 	}
-	
-//	public static void main(String args[]) {
-//		(new GameWindow()).startGame();
-//	}
 }

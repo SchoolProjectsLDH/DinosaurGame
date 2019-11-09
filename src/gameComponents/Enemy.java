8de2c23812f28d56af6a234946e2b2155b6f1ce6
Kggupta <kgupt1@ocdsb.ca>
2019-11-09 09:22:17 -0500

    commented enemy

--- src/gameComponents/Enemy.java
+++ src/gameComponents/Enemy.java
@@ -3,7 +3,7 @@ package gameComponents;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-public abstract class Enemy {
+public abstract class Enemy {//necessary methods (my attempt at abstraction)
     public abstract void updateBaddieLoc();
     public abstract void drawLocation(Graphics g);
     public abstract Rectangle getHitbox();

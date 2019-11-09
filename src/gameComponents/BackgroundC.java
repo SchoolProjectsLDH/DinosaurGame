4c397fd3b35b5643b1c6b9f1c0dacb7ff499dd7e
Kggupta <kgupt1@ocdsb.ca>
2019-11-09 09:08:49 -0500

    Commented clouds file

--- src/gameComponents/BackgroundC.java
+++ src/gameComponents/BackgroundC.java
@@ -10,64 +10,64 @@ import userGUI.GameCreator;
 import zimgHandlers.ImgCompiler;
 
 public class BackgroundC {
-	private final List<ImageCloud> listCloud;
-	private final BufferedImage cloud;
+	private final List<ImageCloud> listCloud;//Cloud types
+	private final BufferedImage cloud;//Cloud Buffer
 	
-	private final Player mainCharacter;
+	private final Player mainCharacter;//Character object
 	
-	public BackgroundC(int width, Player mainCharacter) {
-		this.mainCharacter = mainCharacter;
-		cloud = ImgCompiler.getResouceImage("data/cloud.png");
+	public BackgroundC(int width, Player mainCharacter) {//initialize cloud locations
+		this.mainCharacter = mainCharacter;//transfer mainCharacter data
+		cloud = ImgCompiler.getResouceImage("data/cloud.png");//Cloud image
 		listCloud = new ArrayList<ImageCloud>();
 		
-		ImageCloud imageCloud = new ImageCloud();
-		imageCloud.positionX = 0;
+		ImageCloud imageCloud = new ImageCloud();//new cloud
+		imageCloud.positionX = 0;//Give this some location on the screen
 		imageCloud.positionY = 30;
-		listCloud.add(imageCloud);
+		listCloud.add(imageCloud);//Add to main listcloud
 		
-		imageCloud = new ImageCloud();
+		imageCloud = new ImageCloud();//Comments above
 		imageCloud.positionX = 150;
 		imageCloud.positionY = 40;
 		listCloud.add(imageCloud);
 		
-		imageCloud = new ImageCloud();
+		imageCloud = new ImageCloud();//Comments above
 		imageCloud.positionX = 300;
 		imageCloud.positionY = 50;
 		listCloud.add(imageCloud);
 		
-		imageCloud = new ImageCloud();
+		imageCloud = new ImageCloud();//Comments above
 		imageCloud.positionX = 450;
 		imageCloud.positionY = 20;
 		listCloud.add(imageCloud);
 		
-		imageCloud = new ImageCloud();
+		imageCloud = new ImageCloud();//Comments above
 		imageCloud.positionX = 600;
 		imageCloud.positionY = 60;
 		listCloud.add(imageCloud);
 	}
 	
-	public void moveCloud(){
-		Iterator<ImageCloud> itr = listCloud.iterator();
-		ImageCloud firstElement = itr.next();
-		firstElement.positionX -= mainCharacter.getSpeedX()/8;
-		while(itr.hasNext()) {
+	public void moveCloud(){//Scroll clouds twrd player
+		Iterator<ImageCloud> itr = listCloud.iterator();//switch between cloud locations
+		ImageCloud firstElement = itr.next();//get the next location
+		firstElement.positionX -= mainCharacter.getSpeedX()/8;//depending on speed move position 1/8th of that forward
+		while(itr.hasNext()) {//If there is a next cloud create it at the beginning
 			ImageCloud element = itr.next();
 			element.positionX -= mainCharacter.getSpeedX()/8;
 		}
-		if(firstElement.positionX < -cloud.getWidth()) {
-			listCloud.remove(firstElement);
-			firstElement.positionX = GameCreator.SCREEN_WIDTH;
-			listCloud.add(firstElement);
+		if(firstElement.positionX < -cloud.getWidth()) {//If the cloud is offscreen
+			listCloud.remove(firstElement);//Delete it
+			firstElement.positionX = GameCreator.SCREEN_WIDTH;//Reset its x position
+			listCloud.add(firstElement);//Add it back to the screen
 		}
 	}
 	
 	public void drawNewCloudLoc(Graphics g) {
-		for(ImageCloud imgLand : listCloud) {
-			g.drawImage(cloud, (int) imgLand.positionX, imgLand.positionY, null);
+		for(ImageCloud imgLand : listCloud) {//Draw the clouds
+			g.drawImage(cloud, (int) imgLand.positionX, imgLand.positionY, null);//Draw cloud image to x y coords
 		}
 	}
 	
-	private class ImageCloud {
+	private class ImageCloud {//Attributes of imagecloud
 		float positionX;
 		int positionY;
 	}

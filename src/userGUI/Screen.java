a3105f0cb50c284f6c3e9f50ed959c5eb0b6c3a8
Kggupta <kgupt1@ocdsb.ca>
2019-11-09 10:00:28 -0500

    commented game screen

--- src/userGUI/Screen.java
+++ src/userGUI/Screen.java
@@ -4,97 +4,90 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
 import gameComponents.*;
-import zimgHandlers.ImgCompiler;
 
 public class Screen extends JPanel implements Runnable, KeyListener {
 
-    private final int gameStart = 0;
-    private final int gamePlaying = 1;
-    private final int gameOver = 2;
+    private final int gameStart = 0;//start state
+    private final int gamePlaying = 1;//playing state
+    private final int gameOver = 2;//game over state
 
-    private final Ground land;
-    private final Player mainCharacter;//scorestuff
-    private final EnemyHandler enemiesHandler;
-    private final BackgroundC clouds;
-    private Thread thread;
+    private final Ground land;//new land
+    private final Player mainCharacter;//new player
+    private final EnemyHandler enemiesHandler;//new handler
+    private final BackgroundC clouds;//cloud 
+    private Thread thread;//main game thread
 
     private boolean isKeyPressed;
 
     private int gameState = gameStart;
 
-    private final BufferedImage replayButtonImage;
-    private final BufferedImage gameOverButtonImage;
-    double fps = 100;
-    double msPerFrame = 1000 * 1000000 / fps;
-    public String UserName;
+    protected double fps = 100;
+    protected double msPerFrame = 1000 * 1000000 / fps;
+    protected String UserName;
 
     public Screen(int difficulty, String UserName) {
-        this.UserName = UserName;
-        mainCharacter = new Player();
-        land = new Ground(GameCreator.SCREEN_WIDTH, mainCharacter);
-        mainCharacter.setSpeedX(difficulty);
-        replayButtonImage = ImgCompiler.getResouceImage("data/replay_button.png");
-        gameOverButtonImage = ImgCompiler.getResouceImage("data/gameover_text.png");
-        enemiesHandler = new EnemyHandler(mainCharacter);
-        clouds = new BackgroundC(GameCreator.SCREEN_WIDTH, mainCharacter);
+        this.UserName = UserName;//set username
+        mainCharacter = new Player();//new player obj
+        land = new Ground(GameCreator.SCREEN_WIDTH, mainCharacter);//create the ground
+        mainCharacter.setSpeedX(difficulty);//set the speed of game
+        enemiesHandler = new EnemyHandler(mainCharacter);//start enemy handling with character attributes
+        clouds = new BackgroundC(GameCreator.SCREEN_WIDTH, mainCharacter);//new clouds scrolling
     }
 
     public void startGame() {
-        thread = new Thread(this);
+        thread = new Thread(this);//new game thread
         thread.start();
     }
 
-    public void gameUpdate() {
-        if (gameState == gamePlaying) {
-            clouds.moveCloud();
-            land.updateLandScroll();
-            mainCharacter.updatePlayerState();
-            enemiesHandler.updateEnemyLocations();
-            if (enemiesHandler.isCollided()) {
-                mainCharacter.playDeadOof();
-                gameState = gameOver;
-                mainCharacter.playerDeadState(true);
+    public void gameUpdate() {//game update checker
+        if (gameState == gamePlaying) {//if still playing
+            clouds.moveCloud();//scroll cloud
+            land.updateLandScroll();//scroll land
+            mainCharacter.updatePlayerState();//check player state
+            enemiesHandler.updateEnemyLocations();//scroll enemy
+            if (enemiesHandler.isCollided()) {//if player and enemy intersect
+                mainCharacter.playDeadOof();//play dead sound
+                gameState = gameOver;//game over state
+                mainCharacter.playerDeadState(true);//is dead true
             }
         }
     }
 
     @Override
-    public void paint(Graphics g) {
+    public void paint(Graphics g) {//painting components
         g.setColor(Color.decode("#f7f7f7"));
         g.fillRect(0, 0, getWidth(), getHeight());
 
         switch (gameState) {
-            case gameStart:
-                mainCharacter.drawPlayerComponents(g);
+            case gameStart://on start
+                mainCharacter.drawPlayerComponents(g);//draw player
                 break;
-            case gamePlaying:
-                clouds.drawNewCloudLoc(g);
-                land.draw(g);
-                enemiesHandler.draw(g);
-                mainCharacter.drawPlayerComponents(g);
+            case gamePlaying://on playing
+                clouds.drawNewCloudLoc(g);//cycle cloud scrolling
+                land.draw(g);//land scrolling
+                enemiesHandler.draw(g);//draw enemy
+                mainCharacter.drawPlayerComponents(g);//draw player
                 g.setColor(Color.BLACK);
-                g.drawString("Score: " + mainCharacter.score, 500, 20);
+                g.drawString("Score: " + mainCharacter.score, 500, 20);//place score label
                 break;
-            case gameOver:
+            case gameOver://game end
                 java.awt.EventQueue.invokeLater(new Runnable() {
                     public void run() {
-                        new Lose(mainCharacter.score, UserName).setVisible(true);
+                        new Lose(mainCharacter.score, UserName).setVisible(true);//open lose window
                     }
                 });
-
                 break;
         }
     }
 
     @Override
-    public void run() {
+    public void run() {//main thread
 
-        double lastTime = 0;
+        double lastTime = 0;//various times for speed scaling
         double elapsed;
 
         int msSleep;
@@ -103,15 +96,15 @@ public class Screen extends JPanel implements Runnable, KeyListener {
         long endProcessGame;
         long lag = 0;
 
-        while (gameState!= gameOver) {
-            fps += 0.001;
-            msPerFrame = 1000 * 1000000 / fps;
-            gameUpdate();
-            repaint();
-            endProcessGame = System.nanoTime();
+        while (gameState!= gameOver) {//while you arent dead
+            fps += 0.001;//increment speed
+            msPerFrame = 1000 * 1000000 / fps;//shorten ms per frame
+            gameUpdate();//update all game assets
+            repaint();//repaint the game assets
+            endProcessGame = System.nanoTime();//time for measuring ticks
             elapsed = (lastTime + msPerFrame - System.nanoTime());
             msSleep = (int) (elapsed / 1000000);
-            nanoSleep = (int) (elapsed % 1000000);
+            nanoSleep = (int) (elapsed % 1000000);//sleeping for some time to create the illusion of speed
             if (msSleep <= 0) {
                 lastTime = System.nanoTime();
                 continue;
@@ -126,25 +119,25 @@ public class Screen extends JPanel implements Runnable, KeyListener {
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(KeyEvent e) {//depending on key press
         if (!isKeyPressed) {
             isKeyPressed = true;
             switch (gameState) {
-                case gameStart:
-                    if (e.getKeyCode() == KeyEvent.VK_UP) {
-                        gameState = gamePlaying;
+                case gameStart://game not started yet
+                    if (e.getKeyCode() == KeyEvent.VK_UP) {//key is up
+                        gameState = gamePlaying;// start game
                     }
                     break;
-                case gamePlaying:
-                    if (e.getKeyCode() == KeyEvent.VK_UP) {
-                        mainCharacter.jumpArcMaker();
-                    } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                        mainCharacter.crouchPersist(true);
+                case gamePlaying://game started
+                    if (e.getKeyCode() == KeyEvent.VK_UP) {//key is up
+                        mainCharacter.jumpArcMaker();//jump
+                    } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {//key is down
+                        mainCharacter.crouchPersist(true);//crouch
                     }
                     break;
-                case gameOver:
-                    if (e.getKeyCode() == KeyEvent.VK_UP) {
-                        gameState = gamePlaying;
+                case gameOver://game is over
+                    if (e.getKeyCode() == KeyEvent.VK_UP) {//key up
+                        gameState = gamePlaying;//play again
                         resetGame();
                     }
                     break;
@@ -155,10 +148,10 @@ public class Screen extends JPanel implements Runnable, KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-        isKeyPressed = false;
-        if (gameState == gamePlaying) {
-            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                mainCharacter.crouchPersist(false);
+        isKeyPressed = false;//no key pressed
+        if (gameState == gamePlaying) {//and still playing
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {//stop being crouched
+                mainCharacter.crouchPersist(false);//go back to normal run
             }
         }
     }
@@ -169,7 +162,7 @@ public class Screen extends JPanel implements Runnable, KeyListener {
 
     }
 
-    private void resetGame() {
+    private void resetGame() {//reinit variables
         mainCharacter.playerDeadState(false);
         mainCharacter.returnToLand();
         enemiesHandler.refreshEnemy();

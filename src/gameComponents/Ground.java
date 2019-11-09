dce01d33f525d5c91a2f13135098b3b316e4dbf8
Kggupta <kgupt1@ocdsb.ca>
2019-11-09 09:30:17 -0500

    commented ground file

--- src/gameComponents/Ground.java
+++ src/gameComponents/Ground.java
@@ -11,23 +11,23 @@ import zimgHandlers.ImgCompiler;
 
 public class Ground {
 
-    public static final int landYPosition = 103;
+    public static final int LAND_Y_POS = 103;//Position of land
 
-    private final List<LandImg> compiledLands;
-    private final BufferedImage land1;
+    private final List<LandImg> compiledLands;//All land types
+    private final BufferedImage land1;//type 1
     private final BufferedImage land2;
-    private final BufferedImage land3;
+    private final BufferedImage land3;//type 3
 
-    private final Player mainCharacter;
+    private final Player mainCharacter;//player obj
 
     public Ground(int width, Player mainCharacter) {
-        this.mainCharacter = mainCharacter;
-        land1 = ImgCompiler.getResouceImage("data/land1.png");
+        this.mainCharacter = mainCharacter;//update player attributes
+        land1 = ImgCompiler.getResouceImage("data/land1.png");//Set land paths
         land2 = ImgCompiler.getResouceImage("data/land2.png");
         land3 = ImgCompiler.getResouceImage("data/land3.png");
-        int numberedLand = width / land1.getWidth() + 2;
-        compiledLands = new ArrayList<LandImg>();
-        for(int i = 0; i < numberedLand; i++) {
+        int numberedLand = width / land1.getWidth() + 2;//Start with first
+        compiledLands = new ArrayList<LandImg>();//new arraylist of images
+        for(int i = 0; i < numberedLand; i++) {//Adding all lands to the list
             LandImg imageLand = new LandImg();
             imageLand.positionX = i * land1.getWidth();
             setImageLand(imageLand);
@@ -35,27 +35,27 @@ public class Ground {
         }
     }
 
-    public void updateLandScroll(){
-        Iterator<LandImg> itr = compiledLands.iterator();
+    public void updateLandScroll(){//Scrolling land
+        Iterator<LandImg> itr = compiledLands.iterator();//Get next land type
         LandImg firstElement = itr.next();
-        firstElement.positionX -= mainCharacter.getSpeedX();
-        float previousPosX = firstElement.positionX;
-        while(itr.hasNext()) {
-            LandImg element = itr.next();
-            element.positionX = previousPosX + land1.getWidth();
-            previousPosX = element.positionX;
+        firstElement.positionX -= mainCharacter.getSpeedX();//Change first land based on speed
+        float previousPosX = firstElement.positionX;//Last x pos
+        while(itr.hasNext()) {//as long as there are images in iterator
+            LandImg element = itr.next();//Get a next land image
+            element.positionX = previousPosX + land1.getWidth();//set its x pos
+            previousPosX = element.positionX;//set it as the new prev x (so they are side by side)
         }
-        if(firstElement.positionX < -land1.getWidth()) {
-            compiledLands.remove(firstElement);
-            firstElement.positionX = previousPosX + land1.getWidth();
-            setImageLand(firstElement);
-            compiledLands.add(firstElement);
+        if(firstElement.positionX < -land1.getWidth()) {//If offscreen
+            compiledLands.remove(firstElement);//Delete it
+            firstElement.positionX = previousPosX + land1.getWidth();//Move it to the start
+            setImageLand(firstElement);//Set its image
+            compiledLands.add(firstElement);//Put it back on the list
         }
     }
 
-    private void setImageLand(LandImg imgLand) {
-        int landType = randLandType();
-        switch(landType){
+    private void setImageLand(LandImg imgLand) {//All the different land images
+        int landType = randLandType();//get a random type
+        switch(landType){//get the image path
             case 1:
                 imgLand.image = land1;
                 break;
@@ -68,26 +68,26 @@ public class Ground {
         }
     }
 
-    public void draw(Graphics g) {
-        for(LandImg imgLand : compiledLands) {
-                g.drawImage(imgLand.image, (int) imgLand.positionX, landYPosition, null);
+    public void draw(Graphics g) {//Draw to screen
+        for(LandImg imgLand : compiledLands) {//For all images in compiled land, draw them
+                g.drawImage(imgLand.image, (int) imgLand.positionX, LAND_Y_POS, null);
         }
     }
 
-    private int randLandType() {
-        Random random = new Random();
-        int type = random.nextInt(10);
+    private int randLandType() {//return the land type
+        Random random = new Random();//random obj
+        int type = random.nextInt(10);//set within numbers of 10
         switch (type){
-            case 1:
+            case 1://1 in 10 chance of getting scpecial image
                 return 1;
-            case 9:
+            case 9://1 in 10 chance of getting special image
                 return 3;
-            default:
+            default://basic image
                 return 2;
         }
     }
 
-    private class LandImg {
+    private class LandImg {//attributes of landimg
         float positionX;
         BufferedImage image;
     }

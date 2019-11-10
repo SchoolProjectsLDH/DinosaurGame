374657116fd7b71138ced49666186a3de82c81cd
Fred Chen <fchen3@ocdsb.ca>
2019-11-10 11:42:07 -0500

    Finalized the JSON file handling

--- src/userGUI/ParseJson.java
+++ src/userGUI/ParseJson.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package userGUI;
 
 import java.io.FileNotFoundException;
@@ -19,42 +15,36 @@ import java.nio.charset.StandardCharsets;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-/**
- *
- * @author Emperor Master Chen
- */
+
 public class ParseJson {
 
-    /**
-     *
-     * @author Emperor Master Chen
-     */
+    
     JSONArray highScoresList;
     public boolean isHighScore = false;
     int ranking;
 
     @SuppressWarnings("unchecked")
-    public ParseJson(String path, String playerName, int score) {
-        //JSON parser object to parse read file
-        //JSONParser jsonParser = new JSONParser();
-        StringBuilder contentBuilder = new StringBuilder();
+    public ParseJson(String path, String playerName, int score) { //These are passed from the lose class that occurs once the user dies
+        
+        StringBuilder contentBuilder = new StringBuilder(); //Stringbuilder class allows for a mutable string, i.e. one can append characters
 
-        try (Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        try (Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {//stream creates an object for every line in the file
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));//Lambda appends each stream object to the StringBuilder
 
-            highScoresList = new JSONArray(contentBuilder.toString());
+            highScoresList = new JSONArray(contentBuilder.toString());//A JSON array is created from the .JSON file 
 
-            highScoresList = SortFile(highScoresList);
+            highScoresList = SortFile(highScoresList);//Sorts list so that the lowest score in the file is the last item in array
             if (score > highScoresList.getJSONObject(highScoresList.length() - 1).getInt("Score") || highScoresList.length() < 10) {
+                //^^^ Add the current highscore only if it is greater than the smallest score in the array or the length of the array is less than 10
                 JSONObject newPlayer = new JSONObject("{   \"Score\":" + score + ", \"Name\":" + playerName + "}");
                 isHighScore = true;
                 highScoresList.put(newPlayer);
-                highScoresList.remove(highScoresList.length()-2);
-                highScoresList = SortFile(highScoresList);
-                this.ranking = searchForPlace(highScoresList, score, playerName);
+                highScoresList.remove(highScoresList.length()-2);//Maintains a constant 10 items in array
+                highScoresList = SortFile(highScoresList);//Resorts file with new item added
+                this.ranking = searchForPlace(highScoresList, score, playerName);//Finds the ranking of the current score relative to all the others
             }
 
-            writeToFile(highScoresList, path);
+            writeToFile(highScoresList, path);//Save array to .JSON array
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -66,6 +56,7 @@ public class ParseJson {
     private int searchForPlace(JSONArray arr, int score, String User) {
         for (int i = 0; i < arr.length(); i++) {
             if (highScoresList.getJSONObject(i).getInt("Score") == score && highScoresList.getJSONObject(i).get("Name").toString().equals(User)) {
+                //This makes sure the score is not someone else's and rather the actual player's (since the same score can be very common)
                 return i + 1;
                 
             }
@@ -73,7 +64,7 @@ public class ParseJson {
         return 0;
     }
     
-    public static JSONArray returnArray(String path){
+    public static JSONArray returnArray(String path){//Static since it is simply a conversion function and aids in convience, not used in any instance of a class
         StringBuilder contentBuilder = new StringBuilder();
 
         try (Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
@@ -93,9 +84,8 @@ public class ParseJson {
         for (int i = 0; i < highscores.length(); i++) {
             jsonValues.add(highscores.getJSONObject(i));
         }
-        Collections.sort(jsonValues, new Comparator<JSONObject>() {
-            //You can change "Name" with "ID" if you want to sort by ID
-            private static final String KEY_NAME = "Name";
+        Collections.sort(jsonValues, new Comparator<JSONObject>() {//Compares each item in the JSON Array
+            //You can change "Score" with "User" if you want to sort by ID
 
             @Override
             public int compare(JSONObject a, JSONObject b) {
@@ -105,7 +95,7 @@ public class ParseJson {
                 int valB = (int) b.get("Score");
 
                 if (valA < valB) {
-                    return 1;
+                    return 1;//1, -1, and 0 are used by the comparator to decide whether A or B is larger
                 }
                 if (valA > valB) {
                     return -1;
@@ -123,7 +113,7 @@ public class ParseJson {
 
     }
 
-    public void writeToFile(JSONArray highscores, String path) {
+    public void writeToFile(JSONArray highscores, String path) {//Finalize the highscores by saving them to an actual location
         try (FileWriter file = new FileWriter(path)) {
 
             file.write(highscores.toString());
